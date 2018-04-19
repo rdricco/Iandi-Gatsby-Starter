37619d3bc1d60b8564e4c5a55c11c0371f968001
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-04-19 03:53:33 +0000

    Deploy rdricco/Iandi-Gatsby-Starter to github.com/rdricco/Iandi-Gatsby-Starter.git:gh-pages

--- sw.js
+++ sw.js
@@ -37,7 +37,7 @@
 /* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
 'use strict';
 
-var precacheConfig = [["/app-3ebac6e1b4efa0bde961.js","a20db2aad52f7bf82b5465280d1600f7"],["/commons-bd727ff211fed92ea628.js","7ad9d358b55765443fd757620f305315"],["/index.html","6084e1907e8e39a2c90f48cbfd9f3ef7"],["/manifest.json","4fbccd5d15bd4ba5d3d6177fd498428d"],["/offline-plugin-app-shell-fallback/index.html","77681c17e1aee3b8653201aa277bc802"]];
+var precacheConfig = [["/app-3ebac6e1b4efa0bde961.js","a20db2aad52f7bf82b5465280d1600f7"],["/commons-bd727ff211fed92ea628.js","7ad9d358b55765443fd757620f305315"],["/index.html","6084e1907e8e39a2c90f48cbfd9f3ef7"],["/manifest.json","92715af65f6338626b50f5f62ea324f2"],["/offline-plugin-app-shell-fallback/index.html","77681c17e1aee3b8653201aa277bc802"]];
 var cacheName = 'sw-precache-v3-gatsby-plugin-offline-' + (self.registration ? self.registration.scope : '');
 
 
